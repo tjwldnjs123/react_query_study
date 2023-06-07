2f8f00665c8bdb7cbcce774526b9ca32eee966aa
tjwldnjs123 <tjwldnjs12345@naver.com>
2023-06-07 11:01:56 +0900

    Feat: 리액트쿼리 설치

--- src/components/SuperHeroes.page.js
+++ src/components/SuperHeroes.page.js
@@ -5,11 +5,27 @@ function SuperHeroesPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState([]);
 
-  useEffect(async () => {
-    const res = await axios.get("http://localhost:4000/superheroes");
-    console.log(res);
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get("http://localhost:4000/superheroes");
+      console.log(res.data);
+      setData(res.data);
+      setIsLoading(false);
+    })();
   }, []);
-  return <div>SuperheroesPage</div>;
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>Super Heroes Page</h2>
+      {data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </>
+  );
 }
 
 export default SuperHeroesPage;
